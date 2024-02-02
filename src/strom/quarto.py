9f3c2507a61c17bca1b98e2641c218e88f411b31
edalfon <edalfon@gmail.com>
2024-02-02 16:14:30 +0100

    feat: add basic tracking of file changes before rendering quarto

--- src/strom/quarto.py
+++ src/strom/quarto.py
@@ -5,8 +5,25 @@ from strom.prefect_ops import task_ops
 import subprocess
 import webbrowser
 
+import os
+import hashlib
+import glob
+
+
+# @task(**{**task_ops, "refresh_cache": True})
+def detect_changes(folder_path="quarto", extensions=["qmd", "yml", "css"]):
+
+    file_paths = [
+        file
+        for ext in extensions
+        for file in glob.glob(
+            os.path.join(folder_path, "**", f"*.{ext}"), recursive=True
+        )
+    ]
+    return [hashlib.sha256(open(file, "rb").read()).hexdigest() for file in file_paths]
+
 
 @task(**task_ops)
-def render_report(*args, **kwargs):
+def render_report(*args, c=detect_changes(), **kwargs):
     subprocess.run(["quarto", "render", "quarto", "--execute-dir", "."])
     webbrowser.open("./results/index.html")
